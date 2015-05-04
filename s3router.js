7c716bff9776b28755845b57556dafb13e288ffa
Scott Halstvedt <shalstvedt@gmail.com>
2015-05-03 23:32:29 -0400

    use username as part of filename

--- s3router.js
+++ s3router.js
@@ -37,30 +37,34 @@ function S3Router(options) {
      * give temporary access to PUT an object in an S3 bucket.
      */
     router.get('/sign', function(req, res) {
-        var filename = Math.floor(new Date() / 1000) + "_" + req.query.objectName;
-        var mimeType = mime.lookup(filename);
-        var fileKey = getFileKeyDir(req) + '/' + filename;
-
-        var s3 = new aws.S3();
-        var params = {
-            Bucket: S3_BUCKET,
-            Key: fileKey,
-            Expires: 60,
-            ContentType: mimeType,
-            ACL: 'public-read'
-        };
-        s3.getSignedUrl('putObject', params, function(err, data) {
-            if (err) {
-                console.log(err);
-                return res.send(500, "Cannot create S3 signed URL");
-            }
-            res.json({
-                signedUrl: data,
-                publicUrl: '/s3/img/' + filename,
-		s3Url: 'https://' + S3_BUCKET + '.s3.amazonaws.com/' + fileKey,
-                filename: filename
+        if(!req.user) {
+            res.send(500, "No user is authenticated.");
+        } else {
+	    var filename = Math.floor(new Date() / 1000) + "_" + req.user.username;//req.query.objectName;
+            var mimeType = mime.lookup(filename);
+            var fileKey = getFileKeyDir(req) + '/' + filename;
+	    
+            var s3 = new aws.S3();
+            var params = {
+		Bucket: S3_BUCKET,
+		Key: fileKey,
+		Expires: 60,
+		ContentType: mimeType,
+		ACL: 'public-read'
+            };
+            s3.getSignedUrl('putObject', params, function(err, data) {
+		if (err) {
+                    console.log(err);
+                    return res.send(500, "Cannot create S3 signed URL");
+		}
+		res.json({
+                    signedUrl: data,
+                    publicUrl: '/s3/img/' + filename,
+		    s3Url: 'https://' + S3_BUCKET + '.s3.amazonaws.com/' + fileKey,
+                    filename: filename
+		});
             });
-        });
+	}
     });
 
     return router;

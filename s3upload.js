695ba987d42a01551595d8544625127a228030cc
Maksim Aniskov <MaximAniskov@hotmail.com>
2017-04-26 19:23:59 +0700

    Fix INVALID_STATE_ERR: DOM Exception 11

--- s3upload.js
+++ s3upload.js
@@ -60,10 +60,10 @@ S3Upload.prototype.createCORSRequest = function(method, url, opts) {
     var xhr = new XMLHttpRequest();
 
     if (xhr.withCredentials != null) {
+        xhr.open(method, url, true);
         if (opts.withCredentials != null) {
             xhr.withCredentials = opts.withCredentials;
         }
-        xhr.open(method, url, true);
     }
     else if (typeof XDomainRequest !== "undefined") {
         xhr = new XDomainRequest();

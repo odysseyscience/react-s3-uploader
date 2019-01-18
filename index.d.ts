e2b15e248b8c184adc302ed1bc229712a0d01856
Jake Marsh <jakemmarsh@gmail.com>
2019-01-17 18:25:18 -0800

    update

--- index.d.ts
+++ index.d.ts
@@ -1,31 +1,43 @@
-import { Component } from 'react';
+declare module 'react-s3-uploader' {
+  import { Component } from 'react';
 
-export interface ReactS3UploaderProps {
-  signingUrl?: string;
-  signingUrlMethod?: 'GET' | 'POST';
-  getSignedUrl?: (file: File, callback: (params: { signedUrl: string; }) => any) => any;
-  accept?: string;
-  s3path?: string;
-  preprocess?: (file: File, next: (file: File) => any) => any;
-  onSignedUrl?: (response: object) => any;
-  onProgress?: (percent: number, status: string, file: File) => any;
-  onError?: (message: string) => any;
-  onFinish?: (result: object) => any;
-  signingUrlHeaders?: {
-    additional: object;
-  };
-  signingUrlQueryParams?: {
-    additional: object;
-  };
-  signingUrlWithCredentials?: boolean;
-  uploadRequestHeaders?: object;
-  contentDisposition?: string;
-  server?: string;
-  inputRef?: (ref: HTMLInputElement) => any;
-  autoUpload?: boolean;
-  [key: string]: any;
+  export interface ReactS3UploaderProps {
+    signingUrl?: string;
+    signingUrlMethod?: 'GET' | 'POST';
+    getSignedUrl?: (file: File, callback: (params: { signedUrl: string; }) => any) => any;
+    accept?: string;
+    s3path?: string;
+    preprocess?: (file: File, next: (file: File) => any) => any;
+    onSignedUrl?: (response: object) => any;
+    onProgress?: (percent: number, status: string, file: File) => any;
+    onError?: (message: string) => any;
+    onFinish?: (result: object) => any;
+    signingUrlHeaders?: {
+      additional: object;
+    };
+    signingUrlQueryParams?: {
+      additional: object;
+    };
+    signingUrlWithCredentials?: boolean;
+    uploadRequestHeaders?: object;
+    contentDisposition?: string;
+    server?: string;
+    inputRef?: (ref: HTMLInputElement) => any;
+    autoUpload?: boolean;
+    [key: string]: any;
+  }
+
+  class ReactS3Uploader extends React.Component<ReactS3UploaderProps, any> { }
+
+  export default ReactS3Uploader;
 }
 
-class ReactS3Uploader extends React.Component<ReactS3UploaderProps, any> { }
+declare module 'react-s3-uploader/s3upload' {
+  import { ReactS3UploaderProps } from 'react-s3-uploader';
 
-export default ReactS3Uploader;
+  class S3Upload {
+    constructor(options: ReactS3UploaderProps)
+  }
+
+  export default S3Upload;
+}
